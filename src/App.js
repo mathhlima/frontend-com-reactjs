8037d4ded530e66a1ed839580334df37619c6ca1
Matheus Lima <mathhlima@DESKTOP-1VLQ2DC.localdomain>
2020-08-22 00:33:02 -0300

    add imgs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
+import backgroundImg from  './assets/background.jpg';
 import Header from './components/Header';
 
 function App() {
+    
+    const [projects, setProjects] = useState(['Desenvolvimento de web', 'frontend web']);
+
+    function handleAddProject() {
+        //projects.push(`novo projeto ${Date.now()}`);
+
+        setProjects([...projects, `novo projeto ${Date.now()}`]);
+
+        console.log(projects);
+    }
+    
     return (
         <> 
-            <Header title="home page" >
-                <ul>
-                    <li>home page</li>
-                    <li>projects</li>
-                </ul>
-            </Header>
-            <Header title="projects" >
-                <ul>
-                    <li>home page</li>
-                    <li>projects</li>
-                    <li>login</li>
-                </ul>
-            </Header>
+            <Header title="home page" />
+
+            <img width={300} src={backgroundImg} alt=""/>
+
+            <ul>
+                {projects.map(project => <li key={project}>{project}</li>)}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
         </>
     )
 }
